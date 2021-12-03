c1dfac00b4f6eab832d9b035f16a862a2b1d10f4
Erick1994Intecap <erickruiz1994@gmail.com>
2021-12-03 16:05:21 -0600

    Disenio

--- lib/screens/home_swiped_screen.dart
+++ lib/screens/home_swiped_screen.dart
@@ -41,6 +41,9 @@ class HomeSwippedScreen extends StatelessWidget {
   }
 
   Widget getInfo(BuildContext context, Future<TopRatedResponse> np) {
+    final TextTheme textTheme = Theme.of(context).textTheme;
+    //MEDIA QUERY
+    final size = MediaQuery.of(context).size;
     return Center(
       child: FutureBuilder<TopRatedResponse>(
         future: np,
@@ -54,93 +57,178 @@ class HomeSwippedScreen extends StatelessWidget {
                 itemBuilder: (context, index) {
                   return Center(
                     child: Card(
-                      elevation: 10.0,
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(20.0)),
-                      child: Column(
-                        mainAxisSize: MainAxisSize.min,
-                        children: <Widget>[
-                          ListTile(
-                            trailing: Image.network(
-                                "https://www.themoviedb.org/t/p/w600_and_h900_bestv2" +
-                                    snapshot.data!.results[index]
-                                        .posterPath), //posterPath),
-                            title: Text(snapshot.data!.results[index].title),
-                            subtitle: Text(snapshot
-                                    .data!.results[index].voteAverage
-                                    .toStringAsPrecision(2) +
-                                ' ★'),
-                          ),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.end,
-                            children: <Widget>[
-                              TextButton(
-                                child: const Text('Detalles'),
-                                onPressed: () {
-                                  Navigator.pushNamed(context, 'details',
-                                      arguments: Movie(
-                                          adult: snapshot
-                                              .data!.results[index].adult,
-                                          backdropPath: snapshot.data!
-                                              .results[index].backdropPath,
-                                          id: snapshot.data!.results[index].id,
-                                          originalTitle: snapshot.data!
-                                              .results[index].originalTitle,
-                                          overview: snapshot
-                                              .data!.results[index].overview,
-                                          popularity: snapshot
-                                              .data!.results[index].popularity,
-                                          posterPath: snapshot
-                                              .data!.results[index].posterPath,
-                                          releaseDate: snapshot
-                                              .data!.results[index].releaseDate,
-                                          title: snapshot
-                                              .data!.results[index].title,
-                                          video: snapshot
-                                              .data!.results[index].video,
-                                          voteAverage: snapshot
-                                              .data!.results[index].voteAverage,
-                                          voteCount: snapshot
-                                              .data!.results[index].voteCount));
-                                },
-                              ),
-                              const SizedBox(width: 8),
-                              TextButton(
-                                child: const Text('Similares'),
-                                onPressed: () {
-                                  Navigator.pushNamed(context, 'similar',
-                                      arguments: Movie(
-                                          adult: snapshot
-                                              .data!.results[index].adult,
-                                          backdropPath: snapshot.data!
-                                              .results[index].backdropPath,
-                                          id: snapshot.data!.results[index].id,
-                                          originalTitle: snapshot.data!
-                                              .results[index].originalTitle,
-                                          overview: snapshot
-                                              .data!.results[index].overview,
-                                          popularity: snapshot
-                                              .data!.results[index].popularity,
-                                          posterPath: snapshot
-                                              .data!.results[index].posterPath,
-                                          releaseDate: snapshot
-                                              .data!.results[index].releaseDate,
-                                          title: snapshot
-                                              .data!.results[index].title,
-                                          video: snapshot
-                                              .data!.results[index].video,
-                                          voteAverage: snapshot
-                                              .data!.results[index].voteAverage,
-                                          voteCount: snapshot
-                                              .data!.results[index].voteCount));
-                                },
-                              ),
-                              const SizedBox(width: 8),
-                            ],
-                          ),
-                        ],
-                      ),
-                    ),
+                        elevation: 15.0,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20.0)),
+                        child: Container(
+                            margin: EdgeInsets.only(top: 2),
+                            padding: EdgeInsets.only(bottom: 2),
+                            child: Row(
+                              children: [
+                                Hero(
+                                  tag: snapshot.data!.results[index].id,
+                                  child: ClipRRect(
+                                    borderRadius: BorderRadius.circular(20),
+                                    child: FadeInImage(
+                                      placeholder:
+                                          AssetImage('assets/no_image.jpeg'),
+                                      image: NetworkImage(
+                                          "https://www.themoviedb.org/t/p/w600_and_h900_bestv2" +
+                                              snapshot.data!.results[index]
+                                                  .posterPath),
+                                      height: 150,
+                                    ),
+                                  ),
+                                ),
+                                SizedBox(
+                                  width: 20,
+                                ),
+                                ConstrainedBox(
+                                  constraints: BoxConstraints(
+                                      maxWidth: size.width - 190),
+                                  child: Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Text(
+                                        snapshot.data!.results[index].title,
+                                        style: textTheme.headline5,
+                                        overflow: TextOverflow.ellipsis,
+                                        maxLines: 2,
+                                      ),
+                                      Text(
+                                        snapshot
+                                            .data!.results[index].originalTitle,
+                                        style: textTheme.subtitle1,
+                                        overflow: TextOverflow.ellipsis,
+                                        maxLines: 2,
+                                      ),
+                                      Row(
+                                        children: [
+                                          const Icon(
+                                            Icons.star_outline,
+                                            size: 15,
+                                            color: Colors.cyan,
+                                          ),
+                                          SizedBox(width: 5),
+                                          Text(
+                                              snapshot.data!.results[index]
+                                                  .voteAverage
+                                                  .toStringAsPrecision(2),
+                                              style: textTheme.caption),
+                                        ],
+                                      ),
+                                      Row(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.end,
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.end,
+                                        children: <Widget>[
+                                          TextButton(
+                                            child: const Text('Detalles'),
+                                            onPressed: () {
+                                              Navigator.pushNamed(
+                                                  context, 'details',
+                                                  arguments: Movie(
+                                                      adult: snapshot.data!
+                                                          .results[index].adult,
+                                                      backdropPath: snapshot
+                                                          .data!
+                                                          .results[index]
+                                                          .backdropPath,
+                                                      id: snapshot.data!
+                                                          .results[index].id,
+                                                      originalTitle: snapshot
+                                                          .data!
+                                                          .results[index]
+                                                          .originalTitle,
+                                                      overview: snapshot
+                                                          .data!
+                                                          .results[index]
+                                                          .overview,
+                                                      popularity: snapshot
+                                                          .data!
+                                                          .results[index]
+                                                          .popularity,
+                                                      posterPath: snapshot
+                                                          .data!
+                                                          .results[index]
+                                                          .posterPath,
+                                                      releaseDate: snapshot
+                                                          .data!
+                                                          .results[index]
+                                                          .releaseDate,
+                                                      title: snapshot.data!
+                                                          .results[index].title,
+                                                      video: snapshot.data!
+                                                          .results[index].video,
+                                                      voteAverage: snapshot
+                                                          .data!
+                                                          .results[index]
+                                                          .voteAverage,
+                                                      voteCount: snapshot
+                                                          .data!
+                                                          .results[index]
+                                                          .voteCount));
+                                            },
+                                          ),
+                                          const SizedBox(width: 8),
+                                          TextButton(
+                                            child: const Text('Similares'),
+                                            onPressed: () {
+                                              Navigator.pushNamed(
+                                                  context, 'similar',
+                                                  arguments: Movie(
+                                                      adult: snapshot.data!
+                                                          .results[index].adult,
+                                                      backdropPath: snapshot
+                                                          .data!
+                                                          .results[index]
+                                                          .backdropPath,
+                                                      id: snapshot.data!
+                                                          .results[index].id,
+                                                      originalTitle: snapshot
+                                                          .data!
+                                                          .results[index]
+                                                          .originalTitle,
+                                                      overview: snapshot
+                                                          .data!
+                                                          .results[index]
+                                                          .overview,
+                                                      popularity: snapshot
+                                                          .data!
+                                                          .results[index]
+                                                          .popularity,
+                                                      posterPath: snapshot
+                                                          .data!
+                                                          .results[index]
+                                                          .posterPath,
+                                                      releaseDate: snapshot
+                                                          .data!
+                                                          .results[index]
+                                                          .releaseDate,
+                                                      title: snapshot.data!
+                                                          .results[index].title,
+                                                      video: snapshot.data!
+                                                          .results[index].video,
+                                                      voteAverage: snapshot
+                                                          .data!
+                                                          .results[index]
+                                                          .voteAverage,
+                                                      voteCount: snapshot
+                                                          .data!
+                                                          .results[index]
+                                                          .voteCount));
+                                            },
+                                          ),
+                                          const SizedBox(width: 8),
+                                        ],
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ],
+                            ))),
                   );
                 });
           } else if (snapshot.hasError) {
